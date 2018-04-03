ec89d4987ed35ea12ed11f5afaf22d1c02aa18c9
Erik Jacobson <erik.jacobson@b507.us>
2018-04-03 16:09:41 -0500

    Added middleware to prevent errors

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/question', 'MainController@index')->name('main');
-Route::post('/question/{sentence}/store', 'MainController@store')->name('main.store');
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/question', 'MainController@index')->name('main');
+    Route::post('/question/{sentence}/store', 'MainController@store')->name('main.store');
+    Route::get('/home', 'HomeController@index')->name('home');
+});
