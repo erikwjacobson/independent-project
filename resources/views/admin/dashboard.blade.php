e25e21595c3a1064a684ea50f71d88229824aa6b
Erik Jacobson <erik.jacobson@b507.us>
2018-04-03 20:12:24 -0500

    Useful tidbit of information

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -18,7 +18,17 @@
         <div class="row justify-content-center">
             <div class="col-md-10">
                 <div class="card">
-                    <div class="card-header">Administrator Dashboard</div>
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-md-9">
+                                Administrator Dashboard
+                            </div>
+                            <div class="col-md-3">
+                                <b>Total Participants:</b>&nbsp;{{$participants->count()}}
+                            </div>
+                        </div>
+
+                    </div>
                     <div class="card-body">
                         <div class="row">
                             <div class="col-md-12">
