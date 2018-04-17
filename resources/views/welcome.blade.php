83a4801a5c4c010c2989ac8bb06f7a66d4e2da87
Erik Jacobson <erik.jacobson@b507.us>
2018-04-17 09:05:43 -0500

    Set up a framework for post-production

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,12 +9,48 @@
                         <div class="row">
                             <div class="col-md-12">
                                 <h2>PSYC 211W Research Project</h2>
-                                <p>Completed by: Erik Jacobson, Narissa Gran, Sarah Hagar, Megan Straka, & Molly Burmeister</p>
+                                <p><b>Created by</b>: Erik Jacobson, Narissa Gran, Sarah Hagar, Megan Straka, & Molly Burmeister</p>
                             </div>
                         </div>
                     </div>
                     <div class="card-body">
-
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3>The Study</h3>
+                                <p>
+                                    As part of our research methods course, we were required to develop and execute
+                                    an experimental study relating to the behavioral sciences. We chose to explore the
+                                    topic of emotional interpretation of text messages.
+                                </p>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3>Methodology</h3>
+                                <p>
+                                    Discuss the website
+                                </p>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3>Results</h3>
+                                <p>
+                                    Discuss the findings
+                                </p>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3>Conclusions</h3>
+                                <p>
+                                    Discuss the conclusions
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
