4cfe1bee17688263ee68564d9d693759364d55be
Erik Jacobson <erik@jacobsonfarm.com>
2019-02-28 11:31:19 -0600

    Fixed demo so that there's no access to other parts of the app

---
+++ resources/views/layouts/demo.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" rel="stylesheet" type="text/css">
+
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/extra.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="app">
+    <main class="py-4">
+        @yield('content')
+    </main>
+</div>
+<script src="//twemoji.maxcdn.com/2/twemoji.min.js?11.2"></script>
+<script>
+    $(function() {
+        twemoji.parse(document.body);
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+        })
+    });
+</script>
+@yield('scripts')
+</body>
+</html>
